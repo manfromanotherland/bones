e5eecc9e4cd25c0e8a255c3039d0c5342aece6ad
Edmundo Santos <hello@edmundojr.com>
2016-03-14 16:58:15 +0000

    Update gulpfile.js with better defaults

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,57 @@
+// *************************************
+//  Gulpfile
+// *************************************
+
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
+var browserSync  = require('browser-sync');
 
-// Static Server / Watching files
+// -------------------------------------
+//  Options
+// -------------------------------------
+
+var options = {
+
+	html: {
+		files       : '*.html'
+	},
+
+	sass: {
+		files	    : 'src/scss/**/*.scss',
+		destination : 'dist/css'
+	}
+};
+
+// -------------------------------------
+//  Task: Serve
+// -------------------------------------
+
 gulp.task('serve', ['styles'], function() {
 	browserSync.init({
 		server: './'
 	});
 
-	gulp.watch('src/scss/**/*.scss', ['styles']);
-	gulp.watch('*.html').on('change', browserSync.reload);
+	gulp.watch(options.sass.files, ['styles']);
+	gulp.watch(options.html.files).on('change', browserSync.reload);
 });
 
-// Compile sass into CSS
+// -------------------------------------
+//  Task: Sass
+// -------------------------------------
+
 gulp.task('styles', function() {
-	return gulp.src('src/scss/**/*.scss')
-		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(autoprefixer('last 2 versions', '> 2%'))
-		.pipe(gulp.dest('dist/css/'))
+	return gulp.src(options.sass.files)
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer('last 3 versions'))
+		.pipe(gulp.dest(options.sass.destination))
 		.pipe(browserSync.stream());
 });
+
+// -------------------------------------
+//  Task: Default
+// -------------------------------------
 
 gulp.task('default', ['serve']);
